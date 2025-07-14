4053fd82cccfe0ec0f6906d92953059814563aa7
rich-dev <ricardomtzh.hdz@gmail.com>
2025-07-14 17:11:10 -0600

    BrowserRouter

--- src/rutas/routes.jsx
+++ src/rutas/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/login/login';
 import Principal from '../pages/principal/principal';
 import About from '../pages/about/about';
